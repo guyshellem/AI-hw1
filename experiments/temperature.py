dd7897956eb47c5c169770353e9738e49f516215
guyshellem <guyshellem@gmail.com>
2018-11-20 22:28:08 +0200

    20

--- experiments/temperature.py
+++ experiments/temperature.py
@@ -6,11 +6,12 @@ X = np.array([400, 450, 900, 390, 550])
 # TODO: Write the code as explained in the instructions
 
 alpha = min(X)
-P = []
+prob = []
 T = list(map(lambda x: 0.01+x*(4.99/100), range(101)))
 for t in T:
     sum_of_prob = sum(list(map(lambda x: (x/alpha)**(-1/t), X)))
-    P += [np.array(list(map(lambda x: round(((x/alpha)**(-1/t))/sum_of_prob, 2), X)))]
+    prob += [np.array(list(map(lambda x: round(((x/alpha)**(-1/t))/sum_of_prob, 2), X)))]
+P = np.array(prob)
 
 
 
